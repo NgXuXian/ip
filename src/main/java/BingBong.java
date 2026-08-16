d835874f2fe6b732d2ac50facad184810b570b10
NgXuXian <ngxuxian@gmail.com>
2026-08-16 18:29:28 +0800

    Level-1: Echo

--- src/main/java/BingBong.java
+++ src/main/java/BingBong.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class BingBong {
     public static void main(String[] args) {
         String banner = " ___  _             ___                 \n"
@@ -7,12 +9,35 @@ public class BingBong {
                 + "             |__/                 |__/  \n";
         String horizontalLine = "____________________________________________________________";
 
-        System.out.println(horizontalLine);
         System.out.println(banner);
+        System.out.println(horizontalLine);
         System.out.println("BingBong! What can I do for you?");
+        System.out.println(horizontalLine);
+
+        // Instantiate scanner for user input
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            // Exit when user inputs "bye"
+            if (input.equals("bye")) {
+                break;
+            }
+
+            // Echo user input
+            System.out.println(horizontalLine);
+            System.out.println(input);
+            System.out.println(horizontalLine);
+        }
+
+        // "bye" message
         System.out.println(horizontalLine);
         System.out.println("Bye. BingBong hopes to see you again soon!");
         System.out.println(horizontalLine);
+
+        scanner.close();
     }
 }
+
 
