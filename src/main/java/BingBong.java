16c0d221ce524bc628fce114cda569c506ba3ca4
NgXuXian <ngxuxian@gmail.com>
2026-08-16 18:12:51 +0800

    Level-0: Greet and exit

---
+++ src/main/java/BingBong.java
@@ -0,0 +1,18 @@
+public class BingBong {
+    public static void main(String[] args) {
+        String banner = " ___  _             ___                 \n"
+                + "| _ )(_) _ _  ___  | _ ) ___  _ _  ___  \n"
+                + "| _ \\| || ' \\/ _ \\ | _ \\/ _ \\| ' \\/ _ \\ \n"
+                + "|___/|_||_||_\\_, | |___/\\___/|_||_\\_, | \n"
+                + "             |__/                 |__/  \n";
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(banner);
+        System.out.println("BingBong! What can I do for you?");
+        System.out.println(horizontalLine);
+        System.out.println("Bye. BingBong hopes to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
+
